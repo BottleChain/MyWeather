e44356b02ae3a5516fec724a6b2e63f3681ceed9
LilySu1993 <xiaoyi@ricearmy.com>
2016-09-20 20:37:29 +0800

    init project

--- app/src/main/java/com/yiguy/myweather/MainActivity.java
+++ app/src/main/java/com/yiguy/myweather/MainActivity.java
@@ -1,10 +1,12 @@
 package com.yiguy.myweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+/**
+ * Created by Xiaoyi on 2016/9/20.
+ */
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
