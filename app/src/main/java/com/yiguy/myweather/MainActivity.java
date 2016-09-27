70961f69f8a5f8ef2d76a195f876f83c6b420d36
LilySu1993 <xiaoyi@ricearmy.com>
2016-09-27 21:29:01 +0800

    network finished

--- app/src/main/java/com/yiguy/myweather/MainActivity.java
+++ app/src/main/java/com/yiguy/myweather/MainActivity.java
@@ -1,15 +1,150 @@
 package com.yiguy.myweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.yiguy.util.NetUtil;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Xiaoyi on 2016/9/20.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    //更新按钮
+    private ImageView mUpdateBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK!", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.title_update_btn){
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code","101010100");
+            Log.d("myWeather", cityCode);
+
+            if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+                Log.d("myWeather", "网络OK");
+                queryWeather(cityCode);
+            } else {
+                Log.d("myWeather","网络挂了");
+                Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    /**
+     * 查询某城市的天气信息
+     *  @param cityCode 当前所在城市编码
+     */
+    private void queryWeather(String cityCode){
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("myWeather", address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con = null;
+                try{
+                    URL url = new URL(address);
+                    con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while((str=reader.readLine()) != null){
+                        response.append(str);
+                        Log.d("myWeather",str);
+                    }
+                    String responseStr = response.toString();
+                    Log.d("myWeather", responseStr);
+                    parseXML(responseStr);
+                } catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
+    /**
+     * 解析XML，获取当前城市的天气信息
+     * @param xmldata
+     */
+    private void parseXML(String xmldata){
+        int fengxiangCount = 0;
+        int fengliCount = 0;
+        int dateCount = 0;
+        int highCount = 0;
+        int lowCount = 0;
+        int typeCount = 0;
+        try{
+            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType = xmlPullParser.getEventType();
+            Log.d("myWeather", "pareseXML");
+            while( eventType != XmlPullParser.END_DOCUMENT){
+                switch (eventType){
+                    //判断当前事件是否为文档开始事件
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    //判断当前事件是否为标签元素开始事件
+                    case XmlPullParser.START_TAG:
+                        eventType = xmlPullParser.next();
+                        Log.d("myWeather",xmlPullParser.getName()+":" + xmlPullParser.getText());
+                        break;
+                    //判断当前事件是否为标签元素结束事件
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                //进入下一个元素并触发相应事件
+                eventType = xmlPullParser.next();
+            }
+        } catch (XmlPullParserException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
