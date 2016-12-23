a8ce201c756eadd9fb6f53acc7fc89c500610756
LilySu1993 <1132272672@qq.com>
2016-12-23 13:38:51 +0800

    新增搜索城市的语言实现

--- app/src/main/java/com/yiguy/myweather/SelectCity.java
+++ app/src/main/java/com/yiguy/myweather/SelectCity.java
@@ -3,13 +3,11 @@ package com.yiguy.myweather;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,8 +16,18 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
 import com.yiguy.app.MyApplication;
 import com.yiguy.bean.City;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +38,9 @@ import java.util.List;
 public class SelectCity extends Activity implements View.OnClickListener{
     private static final int FILTER_CITY = 1;
     private ImageView mBackBtn;
+    private ImageView imvRecord;
     private EditText search_edit;
+
     // 城市列表
     private List<City> citys = new ArrayList<City>() ;
     // 选中的城市编号
@@ -121,6 +131,10 @@ public class SelectCity extends Activity implements View.OnClickListener{
         search_edit.addTextChangedListener(mTextWatcher);
         mBackBtn = (ImageView)findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+        imvRecord= (ImageView) findViewById(R.id.imvRecord);
+        imvRecord.setOnClickListener(this);
+
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=585c9bf4");
 
         lvCity = (ListView)findViewById(R.id.lvCity);
 
@@ -163,8 +177,58 @@ public class SelectCity extends Activity implements View.OnClickListener{
                 setResult(RESULT_OK, i);
                 finish();
                 break;
+            case R.id.imvRecord:
+                btnVoice();
+                break;
             default:
                 break;
         }
+    }
+
+    private void btnVoice() {
+        RecognizerDialog dialog = new RecognizerDialog(this,null);
+        dialog.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        dialog.setParameter(SpeechConstant.ACCENT, "mandarin");
+
+        dialog.setListener(new RecognizerDialogListener() {
+            @Override
+            public void onResult(RecognizerResult recognizerResult, boolean b) {
+                printResult(recognizerResult);
+            }
+            @Override
+            public void onError(SpeechError speechError) {
+            }
+        });
+        dialog.show();
+        Toast.makeText(this, "请开始说话", Toast.LENGTH_SHORT).show();
+    }
+
+    //回调结果：
+    private void printResult(RecognizerResult results) {
+        String text = parseIatResult(results.getResultString());
+        if(!text.equals("。") && !text.equals(".") && !text.equals("！")){
+            // 自动填写城市
+            search_edit.setText(text);
+        }
+        //Toast.makeText(SelectCity.this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public static String parseIatResult(String json) {
+        StringBuffer ret = new StringBuffer();
+        try {
+            JSONTokener tokener = new JSONTokener(json);
+            JSONObject joResult = new JSONObject(tokener);
+
+            JSONArray words = joResult.getJSONArray("ws");
+            for (int i = 0; i < words.length(); i++) {
+                // 转写结果词，默认使用第一个结果
+                JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+                JSONObject obj = items.getJSONObject(0);
+                ret.append(obj.getString("w"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret.toString();
     }
 }
